19c0544391e1014749d47c6b61555870d845c82f
Kimsc9976 <tncks097@naver.com>
2023-05-17 01:43:24 +0900

    Refact : 모달 재사용성을 위한 refact

---
+++ Movie_Review_Site/front-server/src/components/modal/ModalButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" :data-bs-target="set_target">
+        테스팅 중 {{ target }}
+    </button>
+</template>
+
+<script>
+export default {
+    name : 'ModalBtn',
+    props:{
+        target : String
+    },
+    computed:{
+        set_target(){
+            return `#${this.target}`
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
