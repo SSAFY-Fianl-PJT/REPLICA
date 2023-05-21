33d7c10ffb900731980e07936e09b72cb4b64a9d
Kimsc9976 <tncks097@naver.com>
2023-05-22 02:05:01 +0900

    Feat : 프로필 추가

--- Movie_Review_Site/front-server/src/views/ProfileView.vue
+++ Movie_Review_Site/front-server/src/views/ProfileView.vue
@@ -1,10 +1,39 @@
 <template>
-  
+  <div v-if="isLogin && get_username">
+    <ProfileItem 
+    :user="get_username"
+    />
+  </div>
+  <div v-else>
+    역기서 나가
+  </div>
+
 </template>
 
 <script>
+import ProfileItem from '@/components/ProfileItem'
 export default {
+    name : 'ProfileView',
+    components:{
+        ProfileItem
+    },
+    deta(){
 
+    },
+    created(){
+        this.$store.dispatch('get_usr_name');
+
+    },
+
+    computed:{
+        isLogin() {
+          return this.$store.getters.isLogin // 로그인 여부
+        },
+        get_username() {
+            if (!this.$store.state.user.info) return false
+            return this.$store.state.user.info.username 
+        },
+    }
 }
 </script>
 
