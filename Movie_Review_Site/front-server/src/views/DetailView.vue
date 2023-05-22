c907d78e3c5f67a94493ae0cc33617b543568753
최영태 <dudxo7721@naver.com>
2023-05-22 16:05:33 +0900

    리뷰 수정/삭제

--- Movie_Review_Site/front-server/src/views/DetailView.vue
+++ Movie_Review_Site/front-server/src/views/DetailView.vue
@@ -1,89 +1,119 @@
 <template>
   <div class="review-information">
-
     <div class="review-datail-container">
       <h1>Detail</h1>
-      {{article.username}}
-      <p>글 번호 : {{ article?.id }}</p>
-      <p>영화 : {{ article?.movie_title }}</p>
-      <p>작성시간 : {{ formatDate(article?.updated_at) }} <span v-if="article?.created_at !== article?.updated_at">(수정됨)</span> </p>
-      <p>제목 : {{ article?.title }}</p>
-      <p>내용 : {{ article?.content }}</p>
-      <!-- <p>작성시간 : {{ article?.created_at }}</p> -->
+      {{ article.username }}
+      <p>글 번호: {{ article?.id }}</p>
+      <p>영화: {{ article?.movie_title }}</p>
+      <p>작성시간: {{ formatDate(article?.updated_at) }} <span v-if="article?.created_at !== article?.updated_at">(수정됨)</span></p>
+      <div v-if="isEditing" @keyup.enter="updateArticle">
+        제목 : <input v-model="editTitle" type="text"><br>
+        내용 : <textarea v-model="editContent"></textarea>
+      </div>
+      <div v-else>
+        <p>제목: {{ article?.title }}</p>
+        <p>내용: {{ article?.content }}</p>
+      </div>
+      <button v-if="!isEditing" @click="startEditing">수정</button>
+      <button v-else @click="updateArticle">수정완료</button>
+      <button @click="deleteArticle">삭제</button>
     </div>
-  
-    <div v-if="movie_detail" class="reive-movie-inform">
-      <MovieItem :item="movie_detail"/>
+
+    <div v-if="movieDetail" class="reive-movie-inform">
+      <MovieItem :item="movieDetail" />
     </div>
   </div>
-
 </template>
 
 <script>
-import {getDetail} from '@/api/article'
-import MovieItem from '@/components/movie/MovieItem.vue'
-import {getMovie_Detail} from '@/api/movie'
+import { getDetail, updateArticle, deleteArticle } from '@/api/article';
+import { getMovieDetail } from '@/api/movie';
+import MovieItem from '@/components/movie/MovieItem.vue';
 
 export default {
   name: 'DetailView',
   data() {
     return {
       article: null,
-      mv_dtail:null
-    }
+      movieDetail: null,
+      isEditing: false,
+      editTitle: '',
+      editContent: '',
+    };
   },
-  components:{
-    MovieItem
+  components: {
+    MovieItem,
   },
   async created() {
-    await this.getArticleDetail()
-    await this.getMovieDetail()
+    await this.getArticleDetail();
+    await this.getMovieDetail();
   },
   methods: {
     async getArticleDetail() {
-
-      await getDetail(this.$route.params.id)
-      .then((res) => {
-        console.log(res)
-        this.article = res.data
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+      const articleId = this.$route.params.id;
+      try {
+        const response = await getDetail(articleId);
+        this.article = response.data;
+        this.editTitle = this.article.title;
+        this.editContent = this.article.content;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getMovieDetail() {
+      const movieId = this.article.movie;
+      try {
+        const response = await getMovieDetail(movieId);
+        this.movieDetail = response.data;
+      } catch (error) {
+        console.error(error);
+      }
     },
     formatDate(dateString) {
       if (!dateString) return '';
       const date = new Date(dateString);
       return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
     },
-    async getMovieDetail(){
-      await getMovie_Detail(this.article.movie)
-      .then((res) =>{
-        console.log("이건 왜 안나옴",res)
-        this.mv_dtail = res.data
-      })
-    }
+    startEditing() {
+      this.isEditing = true;
+    },
+    async updateArticle() {
+      const { id } = this.article;
+      const { editTitle, editContent } = this;
+      try {
+        await updateArticle(id, { movie_title: this.article.movie_title ,title: editTitle, content: editContent });
+        this.isEditing = false;
+        await this.getArticleDetail();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async deleteArticle() {
+      const { id } = this.article;
+      try {
+        await deleteArticle(id);
+        // Redirect to another route or perform other actions after deletion
+        this.$router.push({name: 'ArticleView'});
+      } catch (error) {
+        console.error(error);
+      }
+    },
   },
-  computed:{
-    movie_detail(){
-      return this.mv_dtail
-    }
-  }
-
-}
+};
 </script>
 
 <style scoped>
-.review-information{
+.review-information {
   display: flex;
-  
 }
+
 .review-datail-container {
   margin: 20px;
   text-align: left;
-  flex:1;
+  flex: 1;
 }
-.reive-movie-inform{
-  flex:2;
+
+.reive-movie-inform {
+  flex: 2;
 }
 </style>
