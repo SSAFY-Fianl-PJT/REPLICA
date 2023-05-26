0fbc5195e790df0837f2323867d3449daa3bc168
최영태 <dudxo7721@naver.com>
2023-05-26 11:02:07 +0900

    Fe f

--- Movie_Review_Site/front-server/src/api/utubeapi.js
+++ Movie_Review_Site/front-server/src/api/utubeapi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 // const API_URL = process.env.VUE_APP_API_URL
 const API_URL ='https://www.googleapis.com/youtube/v3/search'
-const YOUTUBE_API_KEY = 'AIzaSyBlNSqg-9WQraWKrdqLdMqh-L0umq_73eA'
+const YOUTUBE_API_KEY = 'AIzaSyBo2A34BLl_MiecOSvnRzOqKEvAUIUDe18'
 
 const api = axios.create({
     baseURL: API_URL,
